3983aecaf000f88d56c657ee9e3644b051b11a11
Jigar Varma <Jvarma5867@conestogac.on.ca>
2023-08-01 13:55:58 -0400

    Created 6 Test Cases

--- RectangleTests/RectangleTest.cs
+++ RectangleTests/RectangleTest.cs
@@ -6,7 +6,91 @@ namespace RectangleTests
 
     public class RectangleTest
     {
-        
+        [Test]
+        public void GetCurrentLength_InitialLength_ReturnsOne()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = 1;
+
+            // Act
+            int actual = rect.GetCurrentLength();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetCurrentLength_Input5_ReturnsNewLength()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = 5;
+            rect.SetNewLength(expected);
+
+            // Act
+            int actual = rect.GetCurrentLength();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetCurrentLength_InputNegative3_ReturnsNegative()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = -3;
+            rect.SetNewLength(expected);
+
+            // Act
+            int actual = rect.GetCurrentLength();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SetNewLength_Input7_ReturnsNewLength()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = 7;
+
+            // Act
+            int actual = rect.SetNewLength(expected);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SetNewLength_Input0_ReturnsZero()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = 0;
+
+            // Act
+            int actual = rect.SetNewLength(expected);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SetNewLength_InputNegative10_ReturnsNegativeLength()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = -10;
+
+            // Act
+            int actual = rect.SetNewLength(expected);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
 
     }
 }
