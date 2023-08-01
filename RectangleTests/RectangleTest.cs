3f6114cda842b7713e61772c902c0e84b7e0dcf9
Jigar Varma <Jvarma5867@conestogac.on.ca>
2023-08-01 14:40:20 -0400

    Created 12 Test Cases

--- RectangleTests/RectangleTest.cs
+++ RectangleTests/RectangleTest.cs
@@ -92,5 +92,90 @@ namespace RectangleTests
             Assert.AreEqual(expected, actual);
         }
 
+        [Test]
+        public void GetCurrentWidth_InitialWidth_ReturnsOne()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = 1;
+
+            // Act
+            int actual = rect.GetCurrentWidth();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetCurrentWidth_Input27_ReturnsNewWidth()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = 27;
+            rect.SetNewWidth(expected);
+
+            // Act
+            int actual = rect.GetCurrentWidth();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetCurrentWidth_InputNegative45_ReturnsNegative()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = -45;
+            rect.SetNewWidth(expected);
+
+            // Act
+            int actual = rect.GetCurrentWidth();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SetNewWidth_Input87_ReturnsNewWidth()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = 87;
+
+            // Act
+            int actual = rect.SetNewWidth(expected);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SetNewWidth_Input0_ReturnsZero()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = 0;
+
+            // Act
+            int actual = rect.SetNewWidth(expected);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SetNewWidth_InputNegative67_ReturnsNegativeWidth()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = -67;
+
+            // Act
+            int actual = rect.SetNewWidth(expected);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
