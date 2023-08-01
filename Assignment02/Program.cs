8226c81f996a455109b5d07e9a5c87215627448f
Jigar Varma <Jvarma5867@conestogac.on.ca>
2023-08-01 15:02:57 -0400

    Created 18 Test Cases

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -108,5 +109,100 @@ namespace Assignment02
             return int.Parse(Console.ReadLine());
 
         }
+
+
+        [Test]
+        public void GetPerimeter_Input4_ReturnsFour()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = 4;
+
+            // Act
+            int actual = rect.GetPerimeter();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetPerimeter_Input5And4_ReturnsNewPerimeter()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            rect.SetNewLength(5);
+            rect.SetNewWidth(4);
+            int expected = 18;
+
+            // Act
+            int actual = rect.GetPerimeter();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetPerimeter_InputNegative3AndNegative90_ReturnsNegativePerimeter()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            rect.SetNewLength(-3);
+            rect.SetNewWidth(-90);
+            int expected = -186;
+
+            // Act
+            int actual = rect.GetPerimeter();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetArea_InitialLengthAndWidth_ReturnsOne()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expected = 1;
+
+            // Act
+            int actual = rect.GetArea();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetArea_Input10And78_ReturnsNewArea()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            rect.SetNewLength(10);
+            rect.SetNewWidth(78);
+            int expected = 780;
+
+            // Act
+            int actual = rect.GetArea();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetArea_InputNegative14AndNegative80_ReturnsPositiveArea()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            rect.SetNewLength(-14);
+            rect.SetNewWidth(-80);
+            int expected = 1120;
+
+            // Act
+            int actual = rect.GetArea();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
+  
